e773767090637b3e8617fd354610b44beb1c0c63
iamapusaha <apusaha392@gmail.com>
2023-11-24 02:32:30 +0600

    setup browser router

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,16 @@
+
+// import PropTypes from 'prop-types';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-xl">this is our home</h1>
+        </div>
+    );
+};
+
+Home.propTypes = {
+
+};
+
+export default Home;
