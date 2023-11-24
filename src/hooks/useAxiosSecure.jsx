ea650e80423c2952128c504f50b8853b6eb6b389
iamapusaha <apusaha392@gmail.com>
2023-11-25 00:48:09 +0600

    secure client with handle error

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,9 +1,17 @@
 import axios from "axios";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
+
+
 
 const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000'
 })
 const useaxiosSecure = () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const { logOut } = useAuth();
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const navigate = useNavigate()
     axiosSecure.interceptors.request.use((config) => {
         const token = localStorage.getItem('access-token');
         config.headers.authorization = `Bearer ${token}`
@@ -11,6 +19,17 @@ const useaxiosSecure = () => {
     }, (error) => {
         return Promise.reject(error)
     })
+
+    // axiosSecure.interceptors.response.use((config) => {
+    //     return config
+    // }, async (error) => {
+    //     const status = error.response.status;
+    //     if (status === 401 || status === 403) {
+    //         logOut();
+    //         navigate('/login')
+    //     }
+    //     return Promise.reject(error)
+    // })
     return axiosSecure;
 };
 
