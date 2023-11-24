fd4e9be0e55165ca4d6cd8157bbeeb88cfaff256
iamapusaha <apusaha392@gmail.com>
2023-11-24 16:33:42 +0600

    after social login show alert,navigate destination

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -2,13 +2,40 @@ import { GoogleAuthProvider } from "firebase/auth";
 // import PropTypes from 'prop-types';
 import { FcGoogle } from "react-icons/fc";
 import useAuth from "../../hooks/useAuth";
+import useAxiousPublic from "../../hooks/useAxiousPublic";
+import Swal from "sweetalert2";
+import { useLocation, useNavigate } from "react-router-dom";
 const SocialLogin = () => {
-    const { signInWithGoogle } = useAuth()
+    const location = useLocation();
+    const navigate = useNavigate()
+    const from = location.state?.from?.pathname || "/";
+    const { signInWithGoogle } = useAuth();
+    const axiousPublic = useAxiousPublic()
     const googleProvider = new GoogleAuthProvider()
     const handleGoogleSigIn = () => {
         signInWithGoogle(googleProvider)
             .then(result => {
-                console.log(result);
+
+                const userInfo = {
+                    name: result.user.displayName,
+                    email: result.user.email
+                }
+                axiousPublic.post('/users', userInfo)
+                    .then(res => {
+                        if (res.data.insertedId) {
+                            Swal.fire({
+                                position: "top-end",
+                                icon: "success",
+                                title: "you are successfully logged in",
+                                showConfirmButton: false,
+                                timer: 1500
+                            });
+                        }
+                        navigate(from, { replace: true })
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    })
             })
             .catch(error => {
                 console.log(error);
