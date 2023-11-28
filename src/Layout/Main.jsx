e6abf3a93846d1fd97a294ca7a11ec9ee404d829
iamapusaha <apusaha392@gmail.com>
2023-11-29 05:17:03 +0600

    added footer

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../pages/Shared/NavBar";
+import Footer from "../pages/Shared/Footer";
 
 
 const Main = () => {
@@ -7,6 +8,7 @@ const Main = () => {
         <div className="container mx-auto">
             <NavBar></NavBar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
