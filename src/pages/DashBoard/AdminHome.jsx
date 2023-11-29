76148edc2a9ddda01ec5dcc285ec498a02660b31
iamapusaha <apusaha392@gmail.com>
2023-11-30 04:22:30 +0600

    added surveyor home

--- src/pages/DashBoard/AdminHome.jsx
+++ src/pages/DashBoard/AdminHome.jsx
@@ -3,7 +3,7 @@
 const AdminHome = () => {
     return (
         <div className="flex justify-center items-center h-screen">
-            <h1 className="text-2xl text-primary">This is our admin home</h1>
+            <h1 className="text-5xl text-primary">This is our admin home</h1>
         </div>
     );
 };
---
+++ src/pages/DashBoard/SurveyorHome.jsx
@@ -0,0 +1,11 @@
+
+
+const SurveyorHome = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <h1 className="text-5xl text-primary">This is our Surveyor home</h1>
+        </div>
+    );
+};
+
+export default SurveyorHome;
