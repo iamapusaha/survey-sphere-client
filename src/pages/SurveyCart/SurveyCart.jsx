ed6b0521fcab147a71d13c3c19da04236942b9e9
iamapusaha <apusaha392@gmail.com>
2023-11-30 18:15:50 +0600

    update totalvote and like on survey cart

--- src/pages/SurveyCart/SurveyCart.jsx
+++ src/pages/SurveyCart/SurveyCart.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 
 // description, category, like, dislike, options
 const SurveyCart = ({ survey }) => {
-    const { _id, description, title, image, totalVotes, yesVotes, noVotes, } = survey;
+    const { _id, description, title, image, totalVote, like } = survey;
 
     const shortDescription = description.length > 40 ? `${description.slice(0, 40)}...` : description;
 
@@ -16,11 +16,11 @@ const SurveyCart = ({ survey }) => {
             {description && <p className="text-gray-700 mb-3">{shortDescription}</p>}
             <div className="flex justify-between items-center my-4">
                 <div>
-                    <span className="text-gray-900 font-bold">{totalVotes}</span>
+                    <span className="text-gray-900 font-bold">{totalVote}</span>
                     <span className="text-gray-600 ml-1">Votes</span>
                 </div>
                 <div>
-                    <span className="text-gray-900 font-bold">{noVotes}</span>
+                    <span className="text-gray-900 font-bold">{like}</span>
                     <span className="text-gray-600 ml-1">Likes</span>
                 </div>
             </div>
