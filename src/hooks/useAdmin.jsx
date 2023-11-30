cdf513f1b47a417e875ff04187c769c66819334f
iamapusaha <apusaha392@gmail.com>
2023-11-30 06:35:45 +0600

    useAdmin update

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -1,27 +1,27 @@
-
 import { useQuery } from "@tanstack/react-query";
-
 import useAxiosSecure from "./useAxiosSecure";
 import { useContext } from "react";
 import { AuthContext } from "../Provider/AuthProvider";
-
 
 const useAdmin = () => {
     const { user, loading } = useContext(AuthContext)
     console.log(user);
     const axiosSecure = useAxiosSecure();
-    const { data: isAdmin, isPending: isAdminLoading } = useQuery({
+    const { data: isAdmin, isPending: isAdminLoading, error } = useQuery({
         queryKey: [user?.email, 'isAdmin'],
         enabled: !loading,
         queryFn: async () => {
-            const res = await axiosSecure.get(`/user/role/${user?.email}`)
-            console.log(res.data);
-            return res.data?.admin;
-
-
+            try {
+                const res = await axiosSecure.get(`/user/role/${user?.email}`)
+                console.log(res.data);
+                return res.data?.admin;
+            } catch (err) {
+                console.error(err);
+                throw new Error('Failed to fetch data');
+            }
         }
     })
-    return [isAdmin, isAdminLoading]
+    return [isAdmin, isAdminLoading, error]
 };
 
 export default useAdmin;
